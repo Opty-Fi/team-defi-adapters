485b6c88b69b237b6d9354bef638c83958bcb311
dhruvin <dhruvin.parikh@georgebrown.ca>
2020-10-05 14:21:15 -0400

    enable/disable/rate liquidity pools

--- contracts/OptyRegistry.sol
+++ contracts/OptyRegistry.sol
@@ -2,95 +2,15 @@
 
 pragma solidity ^0.6.10;
 
-library Address {
-    /**
-     * @dev Returns true if `account` is a contract.
-     *
-     * [IMPORTANT]
-     * ====
-     * It is unsafe to assume that an address for which this function returns
-     * false is an externally-owned account (EOA) and not a contract.
-     *
-     * Among others, `isContract` will return false for the following
-     * types of addresses:
-     *
-     *  - an externally-owned account
-     *  - a contract in construction
-     *  - an address where a contract will be created
-     *  - an address where a contract lived, but was destroyed
-     * ====
-     */
-    function isContract(address account) internal view returns (bool) {
-        // This method relies on extcodesize, which returns 0 for contracts in
-        // construction, since the code is only stored at the end of the
-        // constructor execution.
+import "./utils/Modifiers.sol";
+import "./libraries/Addresses.sol";
 
-        uint256 size;
-        // solhint-disable-next-line no-inline-assembly
-        assembly { size := extcodesize(account) }
-        return size > 0;
-    }
-
-}
-
-/**
- * @dev Contract used to keep all the modifiers at one place
- */
-contract Modifiers {
-    
-    address public owner;
-    address public governance;
-    address public strategist;
-    
-    /**
-     * @dev Sets the owner, governance and strategist while deploying the contract
-     */
-    constructor () internal {
-        owner = msg.sender;
-        governance = msg.sender;
-        strategist = msg.sender;
-    }
-    
-    /**
-     * @dev Modifier to check if the address is zero address or not
-     */
-    modifier onlyValidAddress(){
-        require(msg.sender != address(0), "caller is zero address");
-        _;
-    }
-    
-    /**
-     * @dev Modifier to check caller is owner or not
-     */
-    modifier onlyOwner() {
-        require(msg.sender == owner, "caller is not owner");
-        _;
-    }
-    
-    /**
-     * @dev Modifier to check caller is governance or not
-     */
-    modifier onlyGovernance() {
-        require(msg.sender == governance, "caller is not having governance");
-        _;
-    }
-    
-    /**
-     * @dev Modifier to check caller is strategist or not
-     */
-    modifier onlyStrategist() {
-        require(msg.sender == strategist, "Caller is not strategist");
-        _;
-    }
-}
-
-/**
- * @dev Contract for Opty Strategy Registry
- */
 contract OptyRegistry is Modifiers {
     using Address for address;
     
     mapping(address => bool) public tokens;
+    mapping(address => bool) public liquidityPools;
+    mapping(address => uint) public liquidityPoolRatings;
     
     /**
      * @dev Sets the value for {owner}, {governance} and {strategist}, initializes
@@ -125,7 +45,8 @@ contract OptyRegistry is Modifiers {
      */
     function enableTokens(address _token) public onlyValidAddress onlyOwner onlyGovernance onlyStrategist returns(bool) {
         require(_token != address(0), "token address is a zero address");
-        require(!address(_token).isContract(), "Call to non-contract address");
+        require(address(_token).isContract(), "Call to non-contract address");
+        require(!tokens[_token],"Pool is already enabled");
         tokens[_token] = true;
         return tokens[_token];
     }
@@ -141,9 +62,60 @@ contract OptyRegistry is Modifiers {
      */
     function disableTokens(address _token) public onlyValidAddress onlyOwner onlyGovernance onlyStrategist returns(bool) {
         require(_token != address(0), "Token address is a zero address");
-        require(!address(_token).isContract(), "Call to non-contract address");
+        require(address(_token).isContract(), "Call to non-contract address");
         require(tokens[_token], "Token address doesn't exist");
         tokens[_token] = false;
         return tokens[_token];
+    }
+    /**
+     * @dev Returns the status of the liquidity pool whether it is enabled or not after the function call.
+     * 
+     * Requirements:
+     * 
+     * - the caller should be either owner
+     * - `_poolToken` cannot be a zero address`
+     * - `_poolToken` should be a contract's address and not EOA.
+     */
+    function enableLiquidityPool(address _poolToken) public onlyValidAddress onlyOwner returns(bool) {
+        require(_poolToken != address(0), "poolToken address is a zero address");
+        require(address(_poolToken).isContract(), "Call to non-contract address");
+        require(!liquidityPools[_poolToken],"Pool is already enabled");
+        liquidityPools[_poolToken] = true;
+        return liquidityPools[_poolToken];
+    }
+    
+    /**
+     * @dev Returns the status of the liquidity pool whether it is enabled or not after the function call.
+     * 
+     * Requirements:
+     * 
+     * - the caller should be either owner or governance or strategist
+     * - `_poolToken` cannot be a zero address`
+     * - `_poolToken` should be a contract's address and not EOA.
+     */
+    function disableLiquidityPool(address _poolToken) public onlyValidAddress onlyOwner onlyGovernance onlyStrategist returns(bool) {
+        require(_poolToken != address(0), "poolToken address is a zero address");
+        require(address(_poolToken).isContract(), "Call to non-contract address");
+        require(liquidityPools[_poolToken],"Pool is already disabled");
+        liquidityPools[_poolToken] = false;
+        return liquidityPools[_poolToken];
+    }
+    
+    /**
+     * @dev Returns the rating of the liquidity pool after the function call.
+     * 
+     * Requirements:
+     * 
+     * - the caller should be either owner or governance or strategist
+     * - `_poolToken` cannot be a zero address`
+     * - `_poolToken` should be a contract's address and not EOA.
+     * - `_poolToken` should be enabled
+     */
+    function rateLiquidityPool(address _poolToken, uint _rate) public onlyValidAddress onlyGovernance returns(uint) {
+        require(_poolToken != address(0), "poolToken address is a zero address");
+        require(address(_poolToken).isContract(), "Call to non-contract address");
+        require(liquidityPools[_poolToken],"Pool is not enabled");
+        liquidityPoolRatings[_poolToken] = _rate;
+        return liquidityPoolRatings[_poolToken];
     }
 }
---
+++ contracts/utils/Modifiers.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.6.10;
+
+/**
+ * @dev Contract used to keep all the modifiers at one place
+ */
+contract Modifiers {
+    
+    address public owner;
+    address public governance;
+    address public strategist;
+    
+    /**
+     * @dev Sets the owner, governance and strategist while deploying the contract
+     */
+    constructor () internal {
+        owner = msg.sender;
+        governance = msg.sender;
+        strategist = msg.sender;
+    }
+    
+    /**
+     * @dev Modifier to check if the address is zero address or not
+     */
+    modifier onlyValidAddress(){
+        require(msg.sender != address(0), "caller is zero address");
+        _;
+    }
+    
+    /**
+     * @dev Modifier to check caller is owner or not
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "caller is not owner");
+        _;
+    }
+    
+    /**
+     * @dev Modifier to check caller is governance or not
+     */
+    modifier onlyGovernance() {
+        require(msg.sender == governance, "caller is not having governance");
+        _;
+    }
+    
+    /**
+     * @dev Modifier to check caller is strategist or not
+     */
+    modifier onlyStrategist() {
+        require(msg.sender == strategist, "Caller is not strategist");
+        _;
+    }
+}
+
+/**
+ * @dev Contract for Opty Strategy Registry
+ */
