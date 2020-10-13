0cc027273eed97a9600b561ebf5c7b1ba65544df
Deepanshu Gupta <312.gupta@gmail.com>
2020-10-13 12:01:14 -0400

    - Removed the modifiers from the enable/disable tokens functions (#8)
    * - Removed the modifiers from the enable/disable tokens functions
    - Added new function for checking owner, governance, strategist as OR condition
    - Modified specifications.md by adding new variables used and cosmetic changes for functions inputs/outputs
    
    * - Added deposit DAI and get back opDai as per pool value and total supply
    - Added dev comments for code readability
    
    * `WIP` remove Modifier.sol and rename function
    
    * `WIP` modify OptyRegistry specs
    
    Co-authored-by: dhruvin <dhruvin.parikh@georgebrown.ca>

--- contracts/opty-pools/basic/OptyDAIBasicPool.sol
+++ contracts/opty-pools/basic/OptyDAIBasicPool.sol
@@ -13,10 +13,13 @@ import "./../../utils/Modifiers.sol";
 /**
  * @dev Opty.Fi's Basic Pool contract for DAI token
  */
-contract OptyDAI0Pool is ERC20, ERC20Detailed, Modifiers {
+contract OptyDAIBasicPool is ERC20, ERC20Detailed, Modifiers {
     using SafeERC20 for IERC20;
     
     address public token;   //  store the Dai token contract address
+    // uint256 totalsupply;
+    uint256 private poolValueBefore;
+    uint256 private poolValueAfter;
     
     /**
      * @dev 
@@ -35,13 +38,42 @@ contract OptyDAI0Pool is ERC20, ERC20Detailed, Modifiers {
      *  - Amount should be greater than 0
      *  - Amount is in wad units, Eg: _amount = 1e18 wad means _amount = 1 DAI
      */
-    function invest(uint256 _amount) external nonReentrant returns(bool _success) {
+    function invest(uint256 _amount) external onlyValidAddress nonReentrant returns(bool _success) {
         require(_amount > 0, "deposit must be greater than 0");
         
+        poolValueBefore = calPoolValueInDai();
         IERC20(token).safeTransferFrom(msg.sender, address(this), _amount);
         
+        //  Calculate the shares value for opDai tokens
+        uint256 shares = 0;
+        if (poolValueBefore == 0) {
+            //  Considering 1:1 ratio (Eg: 1 Dai= 1 opDai)
+            shares = _amount;
+        } else {
+            //  Calculating the opDai shares on the basis of totalSupply and poolValue
+            shares = (_amount.mul(totalSupply())).div(poolValueBefore);
+        }
+        poolValueAfter = calPoolValueInDai();
         //  Funtion to mint the opDai tokens for the user equivallent to _amount send as DAI tokens
         _mint(msg.sender, _amount);
         _success = true;
     }
+    
+    /**
+     * @dev Function to calculate pool value in DAI
+     * 
+     * Note:
+     *  - Need to modify this function in future whenever 2nd layer of depositing the DAI into any
+     *    credit pool like compound is added.
+     */
+    function calPoolValueInDai() internal view returns(uint256) {
+        return balance();
+    }
+    
+    /**
+     * @dev Function to get the DAI balance of OptyPool Contract
+     */
+    function balance() public view returns (uint256) {
+        return IERC20(token).balanceOf(address(this));
+  }
 }
