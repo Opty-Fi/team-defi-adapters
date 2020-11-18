216c7ae93283c930bcc887a47bbf088b34128fab
Deepanshu Gupta <312.gupta@gmail.com>
2020-11-18 11:47:12 -0500

    Op 125 (#26)
    * Added code for creating the single step strategies
    
    * skelton for testing  userDeposit()
    
    * - Test case for userDeposit() without rebalance()
    - Added token addresses json file
    - Added Addresses and abis json file
    - Added fundWallet as a helper file to fund the wallet
    - Added .env file getting the mainnet url and private key of the user
    
    * - Added asserts and additional checks for every step
    - Refactored the code for making it generalised
    - Moved  fundWallet() into  utilities file
    - Cosmetic changes into the tokenAddresses.json file
    
    * - Intergrated the help fundWallet() for funcding the wallet with exact number of tokens
    - Added additional check and modified some
    - Removed unwanted code
    
    * - Added test case for userDepositRebalance()
    - Refactored the code for funcding the wallet with the exact number of tokens
    - Tested and worked test script
    - WIP: Convert the userDepositRebalance() test case to more generalised code for reusage
    
    * - Added test script for user deposit rebalance and  tested for aave and compound
    - Refactored it in more general manner for using the strategies
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * removed .package-lock.json and modified .gitignore

--- test/shared/utilities.ts
+++ test/shared/utilities.ts
@@ -1,8 +1,27 @@
-import {
-  BigNumber,
-  bigNumberify,
-} from 'ethers/utils'
+import { BigNumber, bigNumberify } from "ethers/utils";
+import { ethers } from "ethers";
+import exchange from "./exchange.json";
+import tokenAddresses from "./TokenAddresses.json";
 
-export function expandTo18Decimals(n: number): BigNumber {
-  return bigNumberify(n).mul(bigNumberify(10).pow(18))
+export function expandToTokenDecimals(n: number, exponent: number): BigNumber {
+    return bigNumberify(n).mul(bigNumberify(10).pow(exponent));
+}
+
+export async function fundWallet(
+    tokenAddress: string,
+    wallet: ethers.Wallet,
+    amount: string
+) {
+    const uniswapInstance = new ethers.Contract(
+        exchange.uniswap.address,
+        exchange.uniswap.abi,
+        wallet
+    );
+    await uniswapInstance.swapETHForExactTokens(
+        amount,
+        [tokenAddresses.weth, tokenAddress],
+        wallet.address,
+        "1000000000000000000",
+        { value: ethers.utils.hexlify(ethers.utils.parseEther("90")) }
+    );
 }
