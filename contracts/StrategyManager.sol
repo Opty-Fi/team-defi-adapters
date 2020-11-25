07a3c4bdb287c35b3d74901d3739bbb8dc543a39
Deepanshu Gupta <312.gupta@gmail.com>
2020-11-25 18:07:17 -0500

    test script using json format of strategies

--- contracts/StrategyManager.sol
+++ contracts/StrategyManager.sol
@@ -33,17 +33,15 @@ contract StrategyManager is Modifiers{
                         address _account
                         ) public view returns(uint _balance) {
         _balance = 0;
-        if (_hash != 0x0000000000000000000000000000000000000000000000000000000000000000) {
-            StrategyStep[] memory _strategySteps = _getStrategySteps(_hash);
-            require(_strategySteps.length > 0 , "!_strategySteps.length");
-            uint index = _strategySteps.length - 1;
-            if(_strategySteps[index].isBorrow) {
-                // TODO: Return the marke t value of underlying token
-            } else {
-                address _optyPoolProxy = RegistryContract.liquidityPoolToDepositPoolProxy(_strategySteps[index].pool);
-                _balance = IDepositPoolProxy(_optyPoolProxy).
-                            balanceInToken(_underlyingToken,_strategySteps[index].pool,_strategySteps[index].outputToken, _account);
-            }
+        require(_hash != 0x0000000000000000000000000000000000000000000000000000000000000000,"!_hash");
+        StrategyStep[] memory _strategySteps = _getStrategySteps(_hash);
+        require(_strategySteps.length > 0 , "!_strategySteps.length");
+        uint index = _strategySteps.length - 1;
+        if(_strategySteps[index].isBorrow) {
+            // TODO: Return the marke t value of underlying token
+        } else {
+            _balance = IDepositPoolProxy(_strategySteps[index].pool).
+                    balanceInToken(_underlyingToken,_strategySteps[index].pool, _account);
         }
     }
     
@@ -52,7 +50,7 @@ contract StrategyManager is Modifiers{
         uint _amount, 
         bytes32 _hash
         ) public onlyValidAddress returns(bool _success) {
-            require(_hash != 0x0000000000000000000000000000000000000000000000000000000000000000,"!_hash-3");   
+            require(_hash != 0x0000000000000000000000000000000000000000000000000000000000000000,"!_hash");   
             require(_amount > 0 , "!_amount");
             StrategyStep[] memory _strategySteps = _getStrategySteps(_hash);
             uint8 steps = uint8(_strategySteps.length);
@@ -82,7 +80,7 @@ contract StrategyManager is Modifiers{
     }
     
     function poolWithdraw(address _underlyingToken, uint _amount, bytes32 _hash) public onlyValidAddress returns(bool _success) {
-        require(_hash != 0x0000000000000000000000000000000000000000000000000000000000000000,"!_hash-1");   
+        require(_hash != 0x0000000000000000000000000000000000000000000000000000000000000000,"!_hash");   
         require(_amount > 0 , "!_amount");
         StrategyStep[] memory _strategySteps = _getStrategySteps(_hash);
         uint8 steps = uint8(_strategySteps.length);
--- test/aaveLiquidityPools.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "optyAavePoolProxy":"0x112a3Ac907413cc90631B1b695F47cCF4F9e76c6",
-    "aaveLendingPoolAddressProvider":"0x24a42fD28C976A61Df5D00D0599C34c4f90748c8"
-}
