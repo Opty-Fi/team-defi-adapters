d0d4e98d0a397ceda54983a99229f4253a5566f9
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2020-12-16 20:16:38 +0100

    Added paths for harvesting FARM to receive USDC, USDT, TUSD, WBTC or WETH

--- contracts/Gatherer.sol
+++ contracts/Gatherer.sol
@@ -22,11 +22,24 @@ contract Gatherer is Modifiers{
         path[1] = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
         path[2] = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
         setPath(path);
-        path[0] = address(0xa0246c9032bC3A600820415aE600c6388619A14D);
-        setPath(path);
         path[0] = address(0x431ad2ff6a9C365805eBaD47Ee021148d6f7DBe0);
         setPath(path);
         path[0] = address(0xc00e94Cb662C3520282E6f5717214004A7f26888);
+        setPath(path);
+        path[0] = address(0xa0246c9032bC3A600820415aE600c6388619A14D);
+        setPath(path);
+        path[1] = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+        path[2] = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+        setPath(path);
+        path[2] = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+        setPath(path);
+        path[2] = address(0x0000000000085d4780B73119b644AE5ecd22b376);
+        setPath(path);
+        path[2] = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        setPath(path);
+        path = new address[](2);
+        path[0] = address(0xa0246c9032bC3A600820415aE600c6388619A14D);
+        path[1] = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
         setPath(path);
     }
     
