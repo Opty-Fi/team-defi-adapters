162b3246575f96a0475772f1dc9a2a9424f69674
leodinh <leo.atdinh@gmail.com>
2021-04-13 13:13:30 -0400

    added testcase scenario for emergency-brake

--- test/test-opty/emergency-brake.spec.ts
+++ test/test-opty/emergency-brake.spec.ts
@@ -8,21 +8,18 @@ import {
     approveLiquidityPoolAndMapAdapter,
 } from "./setup";
 import { ESSENTIAL_CONTRACTS, CONTRACTS } from "./utils/type";
-import { TOKENS, VAULT, TESTING_CONTRACTS } from "./utils/constants";
+import { TOKENS, TESTING_CONTRACTS } from "./utils/constants";
 import { TypedStrategies } from "./data";
 import {
     getSoliditySHA3Hash,
     fundWalletToken,
     getBlockTimestamp,
 } from "./utils/helpers";
-describe("EmergencyBrake", () => {
+import scenario from "./scenarios/emergency-brake-negative.json";
+describe(scenario.title, () => {
     // TODO: ADD TEST SCENARIOES, ADVANCED PROFILE, STRATEGIES.
     const token = "DAI";
-    const profile = "RP1Vault";
-    const TOKEN_STRATEGY = TypedStrategies[token][profile][0];
-    const MAX_AMOUNT = 10000000;
-    const MIN_AMOUNT = 1;
-    const MAX_JUMP = 100;
+    const MAX_AMOUNT = 100000000;
     let essentialContracts: ESSENTIAL_CONTRACTS;
     let adapters: CONTRACTS;
     let owner: Signer;
@@ -38,156 +35,104 @@ describe("EmergencyBrake", () => {
         }
     });
 
-    for (let i = 0; i < VAULT.length; i++) {
-        if (VAULT[i] === profile) {
-            describe(`${VAULT[i]}`, async () => {
-                let Vault: Contract;
-                const tokensHash = getSoliditySHA3Hash(
-                    ["address[]"],
-                    [[TOKENS[token]]]
-                );
-                let ERC20Instance: Contract;
-                let emergencyBrake: Contract;
-                before(async () => {
-                    try {
-                        Vault = await deployVault(
-                            essentialContracts.registry.address,
-                            essentialContracts.riskManager.address,
-                            essentialContracts.strategyManager.address,
-                            essentialContracts.optyMinter.address,
-                            TOKENS[token],
-                            owner,
-                            admin,
-                            profile
-                        );
-                        await approveLiquidityPoolAndMapAdapter(
-                            essentialContracts.registry,
-                            adapters["CompoundAdapter"].address,
-                            TOKEN_STRATEGY.strategy[0].contract
-                        );
+    for (let i = 0; i < scenario.vaults.length; i++) {
+        describe(`${scenario.vaults[i].name}`, async () => {
+            let Vault: Contract;
+            const vault = scenario.vaults[i];
+            const profile = vault.name;
+            const TOKEN_STRATEGY = TypedStrategies[token][profile][0];
+            const tokensHash = getSoliditySHA3Hash(["address[]"], [[TOKENS[token]]]);
+            let ERC20Instance: Contract;
+            let emergencyBrake: Contract;
+            before(async () => {
+                try {
+                    Vault = await deployVault(
+                        essentialContracts.registry.address,
+                        essentialContracts.riskManager.address,
+                        essentialContracts.strategyManager.address,
+                        essentialContracts.optyMinter.address,
+                        TOKENS[token],
+                        owner,
+                        admin,
+                        profile
+                    );
+                    await approveLiquidityPoolAndMapAdapter(
+                        essentialContracts.registry,
+                        adapters["CompoundAdapter"].address,
+                        TOKEN_STRATEGY.strategy[0].contract
+                    );
 
-                        await setBestBasicStrategy(
-                            TOKEN_STRATEGY.strategy,
-                            tokensHash,
-                            essentialContracts.registry,
-                            essentialContracts.strategyProvider
-                        );
+                    await setBestBasicStrategy(
+                        TOKEN_STRATEGY.strategy,
+                        tokensHash,
+                        essentialContracts.registry,
+                        essentialContracts.strategyProvider
+                    );
 
-                        const timestamp = (await getBlockTimestamp()) * 2;
-                        await fundWalletToken(
-                            TOKENS[token],
-                            owner,
-                            BigNumber.from(MAX_AMOUNT * 100),
-                            timestamp
-                        );
+                    const timestamp = (await getBlockTimestamp()) * 2;
+                    await fundWalletToken(
+                        TOKENS[token],
+                        owner,
+                        BigNumber.from(MAX_AMOUNT * 100),
+                        timestamp
+                    );
 
-                        const EmergencyBrakeFactory = await ethers.getContractFactory(
-                            TESTING_CONTRACTS.TESTING_EMERGENCY_BRAKE_RP1
-                        );
+                    const EmergencyBrakeFactory = await ethers.getContractFactory(
+                        TESTING_CONTRACTS.TESTING_EMERGENCY_BRAKE_RP1
+                    );
 
-                        emergencyBrake = await EmergencyBrakeFactory.deploy(
-                            Vault.address,
-                            TOKENS[token]
-                        );
+                    emergencyBrake = await EmergencyBrakeFactory.deploy(
+                        Vault.address,
+                        TOKENS[token]
+                    );
 
-                        ERC20Instance = await ethers.getContractAt(
-                            "ERC20",
-                            TOKENS[token]
-                        );
-                    } catch (error) {
-                        console.error(error);
-                    }
-                });
-                it("[Double deposit in one block] Should throw an error in DAIRP1Vault with DAI-deposit-COMPOUND-cDAI when the pool value deviates from 1%, maxJumpAllowed=1%", async () => {
-                    try {
-                        await ERC20Instance.connect(owner).transfer(
-                            emergencyBrake.address,
-                            MAX_AMOUNT * 2
-                        );
-                        await Vault.connect(owner).setMaxVaultValueJump(MAX_JUMP);
-                        await emergencyBrake.runDepositRebalance(MIN_AMOUNT);
-                        await expect(
-                            emergencyBrake.runTwoTxnDepositRebalance(
-                                MIN_AMOUNT,
-                                MAX_AMOUNT
-                            )
-                        ).to.be.revertedWith("!maxVaultValueJump");
-                    } catch (err) {
-                        console.log(err);
-                        expect(err.error.message).to.equal(
-                            "VM Exception while processing transaction: revert !maxVaultValueJump"
-                        );
-                    }
-                }).timeout(100000);
-
-                it("[Double withDraw in one block] Should throw an error in DAIRP1Vault with DAI-deposit-COMPOUND-cDAI when the pool value deviates from 1%, maxJumpAllowed=1%", async () => {
-                    try {
-                        await ERC20Instance.connect(owner).transfer(
-                            emergencyBrake.address,
-                            MAX_AMOUNT * 2
-                        );
-                        await Vault.connect(owner).setMaxVaultValueJump(MAX_JUMP);
-                        await emergencyBrake.runDepositRebalance(
-                            MIN_AMOUNT + MAX_AMOUNT
-                        );
-                        await expect(
-                            emergencyBrake.runTwoTxnWithdrawRebalance(
-                                MIN_AMOUNT,
-                                MAX_AMOUNT
-                            )
-                        ).to.be.revertedWith("!maxVaultValueJump");
-                    } catch (err) {
-                        expect(err.error.message).to.equal(
-                            "VM Exception while processing transaction: revert !maxVaultValueJump"
-                        );
-                    }
-                }).timeout(100000);
-
-                it("[Double rebalance in one block] Should throw an error in DAIRP1Vault with DAI-deposit-COMPOUND-cDAI when the pool value deviates from 1%, maxJumpAllowed=1%", async () => {
-                    try {
-                        await ERC20Instance.connect(owner).transfer(
-                            emergencyBrake.address,
-                            MAX_AMOUNT * 2
-                        );
-                        await Vault.connect(owner).setMaxVaultValueJump(MAX_JUMP);
-                        await emergencyBrake.runDepositRebalance(
-                            MIN_AMOUNT + MAX_AMOUNT
-                        );
-                        await expect(
-                            emergencyBrake.runTwoTxnRebalance(MIN_AMOUNT, MAX_AMOUNT)
-                        ).to.be.revertedWith("!maxVaultValueJump");
-                    } catch (err) {
-                        console.log(err);
-                        expect(err.error.message).to.equal(
-                            "VM Exception while processing transaction: revert !maxVaultValueJump"
-                        );
-                    }
-                }).timeout(100000);
-
-                it("[Deposit and withdraw in one block] Should throw an error in DAIRP1Vault with DAI-deposit-COMPOUND-cDAI when the pool value deviates from 1%, maxJumpAllowed=1%", async () => {
-                    try {
-                        await ERC20Instance.connect(owner).transfer(
-                            emergencyBrake.address,
-                            MAX_AMOUNT * 2
-                        );
-                        await Vault.connect(owner).setMaxVaultValueJump(MAX_JUMP);
-                        await emergencyBrake.runDepositRebalance(
-                            MIN_AMOUNT + MAX_AMOUNT
-                        );
-                        await expect(
-                            emergencyBrake.runTwoTxnWithdrawAndDepositRebalance(
-                                MIN_AMOUNT,
-                                MAX_AMOUNT
-                            )
-                        ).to.be.revertedWith("!maxVaultValueJump");
-                    } catch (err) {
-                        console.log(err);
-                        expect(err.error.message).to.equal(
-                            "VM Exception while processing transaction: revert !maxVaultValueJump"
-                        );
-                    }
-                }).timeout(100000);
+                    ERC20Instance = await ethers.getContractAt("ERC20", TOKENS[token]);
+                } catch (error) {
+                    console.error(error);
+                }
             });
-        }
+            beforeEach(async () => {
+                await ERC20Instance.connect(owner).transfer(
+                    emergencyBrake.address,
+                    MAX_AMOUNT * 2
+                );
+                await Vault.connect(owner).setMaxVaultValueJump(vault.maxJump);
+            });
+
+            for (let i = 0; i < vault.stories.length; i++) {
+                const story = vault.stories[i];
+                it(story.description, async () => {
+                    for (let j = 0; j < story.actions.length; j++) {
+                        if (
+                            story.actions[j].action === "runDepositRebalance(uint256)"
+                        ) {
+                            const { amount } = story.actions[j].args;
+                            if (story.actions[j].expect === "success") {
+                                await emergencyBrake[story.actions[j].action](amount);
+                            } else {
+                                await expect(
+                                    emergencyBrake[story.actions[j].action](amount)
+                                ).to.be.revertedWith(story.actions[j].message);
+                            }
+                        } else {
+                            const { max_amount, min_amount } = story.actions[j].args;
+                            if (story.actions[j].expect === "success") {
+                                await emergencyBrake[story.actions[j].action](
+                                    min_amount,
+                                    max_amount
+                                );
+                            } else {
+                                await expect(
+                                    emergencyBrake[story.actions[j].action](
+                                        min_amount,
+                                        max_amount
+                                    )
+                                ).to.be.revertedWith(story.actions[j].message);
+                            }
+                        }
+                    }
+                }).timeout(100000);
+            }
+        });
     }
 });
