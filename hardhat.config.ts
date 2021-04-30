edc7acaad4b800560af8b8226e1f4a6e73791042
Deepanshu Gupta <312.gupta@gmail.com>
2021-04-30 01:13:11 -0400

    fixed env property getting picked as string instead of boolean

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -36,7 +36,7 @@ const buidlerConfig: HardhatUserConfig = {
     gasReporter: {
         currency: "USD",
         gasPrice: 21,
-        enabled: process.env.REPORT_GAS ? true : false,
+        enabled: process.env.REPORT_GAS != "false" ? true : false,
         coinmarketcap: process.env.COINMARKETCAP_API,
     },
 };
