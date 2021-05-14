23f8780b4277f9f1c558e40c1563bc4a8514806f
Deepanshu Gupta <312.gupta@gmail.com>
2021-05-14 13:28:32 -0400

    removed console.log and minor commented code

--- tasks/deployment/deploy-adapters.ts
+++ tasks/deployment/deploy-adapters.ts
@@ -7,11 +7,8 @@ task("deploy-adapters", "Deploy Adapter contracts")
   .setAction(async ({ deployedOnce }, hre) => {
     console.log(`\tDeploying Essential contracts ...`);
     const [owner] = await hre.ethers.getSigners();
-    console.log("Owner: ", owner);
-    // !true ? process.exit(1) : "";
     const essentialContracts: CONTRACTS = await deployEssentialContracts(hre, owner, deployedOnce);
     const essentialContractNames = Object.keys(essentialContracts);
-    console.log("Essential contract names: ", essentialContractNames);
     for (let i = 0; i < essentialContractNames.length; i++) {
       console.log(
         `${essentialContractNames[i].toUpperCase()} address : ${essentialContracts[essentialContractNames[i]].address}`,
@@ -27,7 +24,6 @@ task("deploy-adapters", "Deploy Adapter contracts")
       deployedOnce,
     );
     const adapterContractNames = Object.keys(adaptersContracts);
-    console.log("Adapter Names: ", adapterContractNames);
     for (let i = 0; i < adapterContractNames.length; i++) {
       console.log(
         `${adapterContractNames[i].toUpperCase()} address : ${adaptersContracts[adapterContractNames[i]].address}`,
