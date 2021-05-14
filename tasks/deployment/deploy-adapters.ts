81248f63c53038057875521cd64527b91c588d91
Deepanshu Gupta <312.gupta@gmail.com>
2021-05-14 17:38:42 -0400

    added option for adding deployed contract addresses in DB

--- tasks/deployment/deploy-adapters.ts
+++ tasks/deployment/deploy-adapters.ts
@@ -2,17 +2,28 @@ import { task, types } from "hardhat/config";
 
 import { CONTRACTS } from "../../helpers/type";
 import { deployEssentialContracts, deployAdapters } from "../../helpers/contracts-deployments";
+import { insertContractIntoDB } from "../../helpers/db";
 task("deploy-adapters", "Deploy Adapter contracts")
   .addParam("deployedOnce", "allow checking whether contracts were deployed previously", true, types.boolean)
-  .setAction(async ({ deployedOnce }, hre) => {
-    console.log(`\tDeploying Essential contracts ...`);
+  .addParam("insertindb", "insert the deployed contract addresses in DB", false, types.boolean)
+  .setAction(async ({ deployedOnce, insertindb }, hre) => {
     const [owner] = await hre.ethers.getSigners();
+    console.log(`\tDeploying Essential contracts ...`);
     const essentialContracts: CONTRACTS = await deployEssentialContracts(hre, owner, deployedOnce);
     const essentialContractNames = Object.keys(essentialContracts);
     for (let i = 0; i < essentialContractNames.length; i++) {
       console.log(
         `${essentialContractNames[i].toUpperCase()} address : ${essentialContracts[essentialContractNames[i]].address}`,
       );
+      if (insertindb) {
+        const err = await insertContractIntoDB(
+          essentialContractNames[i],
+          essentialContracts[essentialContractNames[i]].address,
+        );
+        if (err !== "") {
+          console.log(err);
+        }
+      }
     }
     console.log(`\tDeploying Adapter contracts ...`);
     const adaptersContracts: CONTRACTS = await deployAdapters(
@@ -28,5 +39,14 @@ task("deploy-adapters", "Deploy Adapter contracts")
       console.log(
         `${adapterContractNames[i].toUpperCase()} address : ${adaptersContracts[adapterContractNames[i]].address}`,
       );
+      if (insertindb) {
+        const err = await insertContractIntoDB(
+          adapterContractNames[i],
+          adaptersContracts[adapterContractNames[i]].address,
+        );
+        if (err !== "") {
+          console.log(err);
+        }
+      }
     }
   });
