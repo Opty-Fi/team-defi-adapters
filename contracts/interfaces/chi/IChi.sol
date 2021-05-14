59a7e50b111379fc6a96cbe98fbb33e12487292e
leodinh <leo.atdinh@gmail.com>
2021-05-14 01:49:20 -0400

    added CHI token interface

---
+++ contracts/interfaces/chi/IChi.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.10;
+pragma experimental ABIEncoderV2;
+
+/**
+ * @dev Interface of the Opty.fi Vaults.
+ */
+interface IChi {
+    function mint(uint256 value) external;
+}
