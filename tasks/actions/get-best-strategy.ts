2e178bf110dbb6f11d3df923ec6ebb138d4fa711
Leo Tuan Dinh <54964744+leodinh@users.noreply.github.com>
2021-06-18 13:03:40 -0400

    added get and set strategies tasks (#167)

---
+++ tasks/actions/get-best-strategy.ts
@@ -0,0 +1,51 @@
+import { task, types } from "hardhat/config";
+import { getSoliditySHA3Hash } from "../../helpers/utils";
+import { getContractInstance, isAddress } from "../../helpers/helpers";
+import { ESSENTIAL_CONTRACTS, RISK_PROFILES } from "../../helpers/constants";
+
+task("get-best-strategy", "Get best strategy")
+  .addParam("token", "the address of token", "", types.string)
+  .addParam("riskprofile", "risk profile", "", types.string)
+  .addParam("strategyprovider", "the address of strategyProvider", "", types.string)
+  .addParam("isdefault", "whether set best default strategy or not", false, types.boolean)
+  .setAction(async ({ token, riskprofile, strategyprovider, isdefault }, hre) => {
+    if (strategyprovider === "") {
+      throw new Error("strategyprovider cannot be empty");
+    }
+
+    if (!isAddress(strategyprovider)) {
+      throw new Error("strategyprovider address is invalid");
+    }
+
+    if (token === "") {
+      throw new Error("token cannot be empty");
+    }
+
+    if (!isAddress(token)) {
+      throw new Error("token address is invalid");
+    }
+
+    if (riskprofile === "") {
+      throw new Error("riskprofile cannot be empty");
+    }
+
+    if (!Object.keys(RISK_PROFILES).includes(riskprofile.toUpperCase())) {
+      throw new Error("risk profile is not available");
+    }
+
+    const strategyProvider = await getContractInstance(hre, ESSENTIAL_CONTRACTS.STRATEGY_PROVIDER, strategyprovider);
+
+    const tokensHash = getSoliditySHA3Hash(["address[]"], [[token]]);
+
+    try {
+      let strategyHash = "";
+      if (isdefault) {
+        strategyHash = await strategyProvider.rpToTokenToDefaultStrategy(riskprofile.toUpperCase(), tokensHash);
+      } else {
+        strategyHash = await strategyProvider.rpToTokenToBestStrategy(riskprofile.toUpperCase(), tokensHash);
+      }
+      console.log(`StrategyHash : ${strategyHash}`);
+    } catch (error) {
+      console.log(`Got error : `, error.message);
+    }
+  });
