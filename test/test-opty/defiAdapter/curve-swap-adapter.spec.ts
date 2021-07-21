1dccc271c572942f11d62f36507fbacd629a0c7a
dhruvinparikh <imsilent93@gmail.com>
2021-07-21 17:36:33 -0400

    (feat): add curve swap test

--- test/test-opty/defiAdapter/curve-swap-adapter.spec.ts
+++ test/test-opty/defiAdapter/curve-swap-adapter.spec.ts
@@ -1,15 +1,30 @@
 import { expect, assert } from "chai";
 import hre from "hardhat";
-import { Contract, Signer, BigNumber, utils } from "ethers";
+import { Contract, Signer, BigNumber, utils, ethers } from "ethers";
 import { CONTRACTS } from "../../../helpers/type";
 import { TOKENS, TESTING_DEPLOYMENT_ONCE, ZERO_ADDRESS } from "../../../helpers/constants";
-import { TypedAdapterStrategies } from "../../../helpers/data";
-import { deployAdapter, deployEssentialContracts } from "../../../helpers/contracts-deployments";
+import { TypedAdapterStrategies, TypedBtcTokens, TypedDefiPools, TypedTokens } from "../../../helpers/data";
+import {
+  deployAdapter,
+  deployEssentialContracts,
+  deployAdapterPrerequisites,
+  deployAdapters,
+} from "../../../helpers/contracts-deployments";
 import { approveTokens, fundWalletToken, getBlockTimestamp } from "../../../helpers/contracts-actions";
 import scenarios from "../scenarios/adapters.json";
+import testDeFiAdapterScenario from "../scenarios/test-defi-adapter.json";
+import { deployContract } from "../../../helpers/helpers";
+import { getAddress } from "ethers/lib/utils";
 
 type ARGUMENTS = {
   amount?: { [key: string]: string };
+};
+
+type TEST_DEFI_ADAPTER_ARGUMENTS = {
+  maxDepositProtocolPct?: string | null;
+  maxDepositPoolPct?: string | null;
+  maxDepositAmount?: string | null;
+  mode?: string | null;
 };
 
 describe("CurveSwapPoolAdapter", () => {
@@ -178,5 +193,242 @@ describe("CurveSwapPoolAdapter", () => {
         }).timeout(150000);
       }
     });
+  }
+});
+
+describe(`${testDeFiAdapterScenario.title} - CurveSwapPoolAdapter`, () => {
+  let adapterPrerequisites: CONTRACTS;
+  let users: { [key: string]: Signer };
+  const adapterNames = Object.keys(TypedDefiPools);
+  let testDeFiAdapter: Contract;
+  let adapters: CONTRACTS;
+
+  before(async () => {
+    const [owner, admin, user1] = await hre.ethers.getSigners();
+    users = { owner, admin, user1 };
+    adapterPrerequisites = await deployAdapterPrerequisites(hre, owner, true);
+    testDeFiAdapter = await deployContract(hre, "TestDeFiAdapter", false, users["owner"], []);
+    adapters = await deployAdapters(hre, owner, adapterPrerequisites.registry.address, true);
+  });
+
+  const ValidatedBtcTokens = Object.values(TypedBtcTokens).map(t => getAddress(t));
+  for (const adapterName of adapterNames) {
+    // TODO: In future it can be leverage across all the adapters
+    if (adapterName == "CurveSwapPoolAdapter") {
+      const pools = Object.keys(TypedDefiPools[adapterName]);
+      for (const pool of pools) {
+        const underlyingTokenAddress = getAddress(TypedDefiPools[adapterName][pool].tokens[0]);
+        // TODO: Refactor fundWalletToken() to get USDK,LINKUSD,SBTC, HBTC, 3Crv
+        if (
+          TypedDefiPools[adapterName][pool].tokens.length == 1 &&
+          getAddress(underlyingTokenAddress) != getAddress(TypedTokens.USDK) &&
+          getAddress(underlyingTokenAddress) != getAddress(TypedTokens.LINKUSD) &&
+          getAddress(underlyingTokenAddress) != getAddress(TypedTokens.SBTC) &&
+          getAddress(underlyingTokenAddress) != getAddress(TypedTokens.HBTC) &&
+          getAddress(underlyingTokenAddress) != getAddress(TypedTokens.THREE_CRV)
+        ) {
+          for (const story of testDeFiAdapterScenario.stories) {
+            it(`${pool} - ${story.description}`, async () => {
+              let defaultFundAmount: BigNumber = ValidatedBtcTokens.includes(underlyingTokenAddress)
+                ? BigNumber.from("1")
+                : BigNumber.from("20000");
+              defaultFundAmount =
+                underlyingTokenAddress == getAddress(TypedTokens.DUSD) ||
+                underlyingTokenAddress == getAddress(TypedTokens.HUSD) ||
+                underlyingTokenAddress == getAddress(TypedTokens.MUSD)
+                  ? BigNumber.from("2000")
+                  : defaultFundAmount;
+              let limit: BigNumber;
+              const timestamp = (await getBlockTimestamp(hre)) * 2;
+              const liquidityPool = TypedDefiPools[adapterName][pool].pool;
+              const ERC20Instance = await hre.ethers.getContractAt("ERC20", underlyingTokenAddress);
+              const decimals = await ERC20Instance.decimals();
+              const adapterAddress = adapters[adapterName].address;
+              let underlyingBalanceBefore: BigNumber = ethers.BigNumber.from(0);
+              let limitInUnderlyingToken: BigNumber = ethers.BigNumber.from(0);
+              for (const action of story.setActions) {
+                switch (action.action) {
+                  case "setMaxDepositProtocolMode(uint8)": {
+                    const { mode } = action.args as TEST_DEFI_ADAPTER_ARGUMENTS;
+                    const existingMode = await adapters[adapterName].maxDepositProtocolMode();
+                    if (existingMode != mode) {
+                      await adapters[adapterName][action.action](mode);
+                    }
+                    break;
+                  }
+                  case "setMaxDepositProtocolPct(uint256)": {
+                    const existingPoolPct: BigNumber = await adapters[adapterName].maxDepositPoolPct(liquidityPool);
+                    if (!existingPoolPct.eq(BigNumber.from(0))) {
+                      await adapters[adapterName].setMaxDepositPoolPct(liquidityPool, 0);
+                    }
+                    const { maxDepositProtocolPct } = action.args as TEST_DEFI_ADAPTER_ARGUMENTS;
+                    const existingProtocolPct: BigNumber = await adapters[adapterName].maxDepositProtocolPct();
+                    if (!existingProtocolPct.eq(BigNumber.from(maxDepositProtocolPct))) {
+                      await adapters[adapterName][action.action](maxDepositProtocolPct);
+                    }
+                    // Note: The pool value for curve pools will be in USD or BTC
+                    const poolValue: BigNumber = await adapters[adapterName].getPoolValue(
+                      liquidityPool,
+                      underlyingTokenAddress,
+                    );
+                    limit = poolValue.mul(BigNumber.from(maxDepositProtocolPct)).div(BigNumber.from(10000));
+                    limitInUnderlyingToken = limit.div(BigNumber.from(10).pow(BigNumber.from(18).sub(decimals)));
+                    defaultFundAmount = defaultFundAmount.mul(BigNumber.from(10).pow(decimals));
+                    defaultFundAmount = defaultFundAmount.lte(limitInUnderlyingToken)
+                      ? defaultFundAmount
+                      : limitInUnderlyingToken;
+                    break;
+                  }
+                  case "setMaxDepositPoolPct(address,uint256)": {
+                    const { maxDepositPoolPct } = action.args as TEST_DEFI_ADAPTER_ARGUMENTS;
+                    const existingPoolPct: BigNumber = await adapters[adapterName].maxDepositPoolPct(liquidityPool);
+                    if (!existingPoolPct.eq(BigNumber.from(maxDepositPoolPct))) {
+                      await adapters[adapterName][action.action](liquidityPool, maxDepositPoolPct);
+                    }
+                    // Note: The pool value for curve pools will be in USD or BTC
+                    const poolValue: BigNumber = await adapters[adapterName].getPoolValue(
+                      liquidityPool,
+                      underlyingTokenAddress,
+                    );
+                    limit = poolValue.mul(BigNumber.from(maxDepositPoolPct)).div(BigNumber.from(10000));
+                    limitInUnderlyingToken = limit.div(BigNumber.from(10).pow(BigNumber.from(18).sub(decimals)));
+                    defaultFundAmount = defaultFundAmount.mul(BigNumber.from(10).pow(decimals));
+                    defaultFundAmount = defaultFundAmount.lte(limitInUnderlyingToken)
+                      ? defaultFundAmount
+                      : limitInUnderlyingToken;
+                    break;
+                  }
+                  case "setMaxDepositAmount(address,address,uint256)": {
+                    // Note: for curve maxDepositAmount will be in USD or BTC
+                    const { maxDepositAmount } = action.args as TEST_DEFI_ADAPTER_ARGUMENTS;
+                    const existingDepositAmount: BigNumber = await adapters[adapterName].maxDepositAmount(
+                      liquidityPool,
+                    );
+                    if (
+                      !existingDepositAmount.eq(
+                        BigNumber.from(maxDepositAmount).mul(BigNumber.from(10).pow(BigNumber.from(18))),
+                      )
+                    ) {
+                      await adapters[adapterName][action.action](
+                        liquidityPool,
+                        underlyingTokenAddress,
+                        BigNumber.from(maxDepositAmount).mul(BigNumber.from(10).pow(BigNumber.from(18))),
+                      );
+                    }
+                    const updatedDepositAmount: BigNumber = await adapters[adapterName].maxDepositAmount(liquidityPool);
+                    limitInUnderlyingToken = BigNumber.from(updatedDepositAmount).div(
+                      BigNumber.from(10).pow(BigNumber.from(18).sub(decimals)),
+                    );
+                    defaultFundAmount = defaultFundAmount.mul(BigNumber.from(10).pow(decimals));
+                    defaultFundAmount = defaultFundAmount.lte(limitInUnderlyingToken)
+                      ? defaultFundAmount
+                      : limitInUnderlyingToken;
+                    break;
+                  }
+                  case "fundTestDeFiAdapterContract": {
+                    const underlyingBalance: BigNumber = await ERC20Instance.balanceOf(testDeFiAdapter.address);
+                    if (underlyingBalance.lt(defaultFundAmount)) {
+                      try {
+                        await fundWalletToken(
+                          hre,
+                          underlyingTokenAddress,
+                          users["owner"],
+                          defaultFundAmount,
+                          timestamp,
+                          testDeFiAdapter.address,
+                        );
+                      } catch (error) {
+                        console.log(
+                          `${pool} - ${story.description} ${ethers.utils.formatUnits(defaultFundAmount, decimals)}`,
+                          error,
+                        );
+                      }
+                    }
+
+                    break;
+                  }
+                  case "testGetDepositAllCodes(address,address,address)": {
+                    underlyingBalanceBefore = await ERC20Instance.balanceOf(testDeFiAdapter.address);
+                    await testDeFiAdapter[action.action](underlyingTokenAddress, liquidityPool, adapterAddress);
+                    break;
+                  }
+                }
+              }
+              for (const action of story.getActions) {
+                switch (action.action) {
+                  case "getLiquidityPoolTokenBalance(address,address,address)": {
+                    const lpTokenBalance = await adapters[adapterName][action.action](
+                      testDeFiAdapter.address,
+                      underlyingTokenAddress,
+                      liquidityPool,
+                    );
+                    const existingMode = await adapters[adapterName].maxDepositProtocolMode();
+                    if (existingMode == 0) {
+                      const existingDepositAmount: BigNumber = await adapters[adapterName].maxDepositAmount(
+                        liquidityPool,
+                      );
+                      if (existingDepositAmount.eq(0)) {
+                        expect(lpTokenBalance).to.be.eq(0);
+                      } else {
+                        expect(lpTokenBalance).to.be.gt(0);
+                      }
+                    } else {
+                      const existingPoolPct: BigNumber = await adapters[adapterName].maxDepositPoolPct(liquidityPool);
+                      const existingProtocolPct: BigNumber = await adapters[adapterName].maxDepositProtocolPct();
+                      if (existingPoolPct.eq(0) && existingProtocolPct.eq(0)) {
+                        expect(lpTokenBalance).to.be.eq(0);
+                      } else if (!existingPoolPct.eq(0) || !existingProtocolPct.eq(0)) {
+                        expect(lpTokenBalance).to.be.gt(0);
+                      }
+                    }
+                    break;
+                  }
+                  case "balanceOf(address)": {
+                    const underlyingBalanceAfter: BigNumber = await ERC20Instance[action.action](
+                      testDeFiAdapter.address,
+                    );
+                    if (underlyingBalanceBefore.lt(limitInUnderlyingToken)) {
+                      expect(underlyingBalanceAfter).to.be.eq(0);
+                    } else {
+                      expect(underlyingBalanceAfter.div(BigNumber.from(10).pow(BigNumber.from(decimals)))).to.be.eq(
+                        underlyingBalanceBefore
+                          .sub(limitInUnderlyingToken)
+                          .div(BigNumber.from(10).pow(BigNumber.from(decimals))),
+                      );
+                    }
+                    break;
+                  }
+                }
+              }
+              for (const action of story.cleanActions) {
+                switch (action.action) {
+                  case "testGetWithdrawAllCodes(address,address,address)": {
+                    await testDeFiAdapter[action.action](underlyingTokenAddress, liquidityPool, adapterAddress);
+                    break;
+                  }
+                }
+              }
+              for (const action of story.getActions) {
+                switch (action.action) {
+                  case "getLiquidityPoolTokenBalance(address,address,address)": {
+                    const lpTokenBalance = await adapters[adapterName][action.action](
+                      testDeFiAdapter.address,
+                      underlyingTokenAddress,
+                      liquidityPool,
+                    );
+                    expect(lpTokenBalance).to.be.eq(0);
+                    break;
+                  }
+                  case "balanceOf(address": {
+                    const underlyingBalance: BigNumber = await ERC20Instance.balanceOf(testDeFiAdapter.address);
+                    expect(underlyingBalance).to.be.gt(0);
+                  }
+                }
+              }
+            });
+          }
+        }
+      }
+    }
   }
 });
