fe152974059fcbb725b820565769a7578750ce11
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2021-07-22 15:01:50 +0200

    Removed unnecessary import in fulcrum-adapter.spec.ts

--- test/test-opty/defiAdapter/fulcrum-adapter.spec.ts
+++ test/test-opty/defiAdapter/fulcrum-adapter.spec.ts
@@ -3,7 +3,7 @@ import hre from "hardhat";
 import { Contract, Signer, BigNumber, utils } from "ethers";
 import { CONTRACTS } from "../../../helpers/type";
 import { TOKENS, TESTING_DEPLOYMENT_ONCE } from "../../../helpers/constants";
-import { TypedAdapterStrategies, TypedBtcTokens, TypedDefiPools } from "../../../helpers/data";
+import { TypedAdapterStrategies, TypedDefiPools } from "../../../helpers/data";
 import {
   deployAdapter,
   deployEssentialContracts,
