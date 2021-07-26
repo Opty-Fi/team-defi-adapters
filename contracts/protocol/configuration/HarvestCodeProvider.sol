92ece7780c4ed924ee64fabbc1d01c40c39ec072
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2021-07-26 22:14:06 +0200

    Added constant keyword in some HarvestCodeProvider.sol variables

--- contracts/protocol/configuration/HarvestCodeProvider.sol
+++ contracts/protocol/configuration/HarvestCodeProvider.sol
@@ -29,12 +29,12 @@ contract HarvestCodeProvider is IHarvestCodeProvider, Modifiers {
     /**
      * @notice Uniswap V2 router contract address
      */
-    address public uniswapV2Router02 = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    address public constant uniswapV2Router02 = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
 
     /**
      * @notice Sushiswap router contract address
      */
-    address public sushiswapRouter = address(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);
+    address public constant sushiswapRouter = address(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);
 
     /**
      * @notice SUSHI token contract address
