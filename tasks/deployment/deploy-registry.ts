994fc24486cf0fcdb916131d187089dad74bbb4a
leodinh <leo.atdinh@gmail.com>
2021-08-25 13:50:49 -0400

    added task-names file

--- tasks/deployment/deploy-registry.ts
+++ tasks/deployment/deploy-registry.ts
@@ -1,8 +1,9 @@
 import { task, types } from "hardhat/config";
 import { deployRegistry } from "../../helpers/contracts-deployments";
 import { insertContractIntoDB } from "../../helpers/db";
+import { DEPLOY_REGISTRY } from "../task-names";
 
-task("deploy-registry", "Deploy Registry")
+task(DEPLOY_REGISTRY, "Deploy Registry")
   .addParam("deployedonce", "allow checking whether contracts were deployed previously", true, types.boolean)
   .addParam("insertindb", "allow inserting to database", false, types.boolean)
   .setAction(async ({ deployedonce, insertindb }, hre) => {
