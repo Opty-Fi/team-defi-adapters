4b5702d3e59be013dd5d2d733d8a78809d69b1f0
leodinh <leo.atdinh@gmail.com>
2021-09-28 11:57:04 -0400

    replaced 3crv with three_crv

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -130,7 +130,7 @@ export function getDefaultFundAmountInDecimal(underlyingTokenAddress: string, de
     case getAddress(TypedTokens.RSV):
     case getAddress(TypedTokens.YCRV):
     case getAddress(TypedTokens.ESD):
-    case getAddress(TypedTokens["3CRV"]):
+    case getAddress(TypedTokens.THREE_CRV):
     case getAddress(TypedTokens.LINK_YLINK): {
       defaultFundAmount = BigNumber.from("20").mul(to_10powNumber_BN(decimal));
       break;
