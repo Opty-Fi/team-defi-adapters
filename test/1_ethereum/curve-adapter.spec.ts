3c98f2d7e075f638f78ad1a922f33f39343c2108
dhruvinparikh <imsilent93@gmail.com>
2021-11-27 23:03:14 -0500

    fix(curve): linkusd pools

--- test/1_ethereum/curve-adapter.spec.ts
+++ test/1_ethereum/curve-adapter.spec.ts
@@ -468,6 +468,10 @@ describe("CurveAdapters Unit test", () => {
                         console.log("skipping as underlying token is not a vault token");
                         this.skip();
                       }
+                      if (rewardTokenAddress == ADDRESS_ZERO) {
+                        console.log("skipping as the pool does not have reward token");
+                        this.skip();
+                      }
 
                       underlyingBalanceBefore = await ERC20Instance.balanceOf(testDeFiAdapter.address);
                       rewardTokenBalanceBefore = await rewardTokenInstance.balanceOf(testDeFiAdapter.address);
@@ -490,6 +494,10 @@ describe("CurveAdapters Unit test", () => {
                     case "testGetHarvestSomeCodes(address,address,address,uint256)": {
                       if (!vaultUnderlyingTokens.includes(getAddress(underlyingTokenAddress))) {
                         console.log("Skipping as underlying token is not vault token");
+                        this.skip();
+                      }
+                      if (rewardTokenAddress == ADDRESS_ZERO) {
+                        console.log("skipping as the pool does not have reward token");
                         this.skip();
                       }
                       underlyingBalanceBefore = await ERC20Instance.balanceOf(testDeFiAdapter.address);
